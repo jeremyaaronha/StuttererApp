068dba3e8f5c3a5edee719e54c324340337501a0
jeremyaaronha <jeremyaaronha@gmail.com>
2026-04-13 21:43:17 -0400

    Initial Commit

---
+++ StuttererApp/StuttererAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StuttererAppApp.swift
+//  StuttererApp
+//
+//  Created by Jeremy Aaron Herrera Arévalo on 13-04-26.
+//
+
+import SwiftUI
+
+@main
+struct StuttererAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
